7015f84ad18940098771135c2fbfba96c9605ce0
Kuntal Pramanik <kuntalpramanik690@gmail.com>
2025-08-09 01:18:17 +0530

    odd even

---
+++ oddeven.py
@@ -0,0 +1,14 @@
+num=int(input("enter a number"))
+bi=bin(num)
+if bi[-1]=='0':
+    print("Even")
+else:
+    print("Odd")
+
+num=int(input("enter a number"))
+bi=bin(num)
+if num%2==0:
+    print("Even")
+else:
+    print("odd")
+
